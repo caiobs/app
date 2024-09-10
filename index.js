188369e64d461d6a8a542d63f3c88aff181dc4d2
Caio Brandão <caiobrandaos@hotmail.com>
2024-09-10 15:27:04 -0300

    Adiciona o inquirer

--- index.js
+++ index.js
@@ -1,48 +1,15 @@
-// olá, mundo!
-console.log("olá, mundo!")
+const start = () => {
+    while(true) {
+        let opcao = "sair";
+        switch(opcao) {
+            case "cadastrar":
+                console.log("Vamos cadastrar.");
+            case "listar":
+                console.log("Vamos listar.");
+            case "sair":
+                return;
+        };
+    };
+};
 
-
-// variáveis - let
-let mensagens = "olá, mundo!"
-
-
-// variáveis - const
-const mensagem = "olá, eu"
-{
-    const mensagem = "olá, Mayk!"
-    console.log(mensagem)
-}
-console.log(mensagem);
-
-
-// arrays
-let metas = ['mayk', 'alô']
-let metas01 = [2, 'mayk']
-// concatenando valores
-console.log(metas[1] + ", " + metas[0]) 
-
-let metas02 = [
-    metas,
-    {
-        value: 'caminhar 20 minutos todos os dias',
-        checked: false
-    }
-]
-console.log(metas02[1].value);
-
-// objetos
-let meta = {
-    value: 'ler um livro por mês',
-    address: 2,
-    checked: true,
-    isChecked: () => {
-        console.log(info)
-    }
-}
-console.log(meta.value);
-
-// function // arrow function
-const criarMeta = () => {}
-
-// named function
-function criarMetas() {}
+start();
